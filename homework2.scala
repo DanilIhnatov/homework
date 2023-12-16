c4ec76c3c7258e812885fb3d82396a45b3ac11f7
DanilIhnatov <153916786+DanilIhnatov@users.noreply.github.com>
2023-12-16 13:55:55 +0200

    Add files via upload

---
+++ homework2.scala
@@ -0,0 +1,71 @@
+package karazin.scala.users.group.week2.homework
+
+import scala.annotation.targetName
+import scala.math.{abs, signum}
+
+object Homework:
+
+  // `x` and `y` are inaccessible from outside
+  class Rational(x: Int, y: Int):
+    // Checking the precondition. Is fails then throws `IllegalArgumentException`
+    require(y > 0, "Denominator must be positive")
+
+    def this(x: Int) = this(x, 1)
+
+    val numer = x / g
+    val denom = y / g
+
+    // Defines an external name for a definition
+    @targetName("less than")
+    // Annotation on a method definition allows using the method as an infix operation
+    infix def <(that: Rational): Boolean =
+      this.numer * that.denom < that.numer * this.denom
+
+    @targetName("less or equal")
+    infix def <=(that: Rational): Boolean =
+      this < that || this == that
+
+    @targetName("greater than")
+    infix def >(that: Rational): Boolean =
+      !(this <= that)
+
+    @targetName("greater or equal")
+    infix def >=(that: Rational): Boolean =
+      !(this < that)
+
+    @targetName("addition")
+    infix def +(that: Rational): Rational =
+      Rational(this.numer * that.denom + that.numer * this.denom, this.denom * that.denom)
+
+    @targetName("negation")
+    infix def unary_- : Rational =
+      Rational(-this.numer, this.denom)
+
+    @targetName("substraction")
+    infix def -(that: Rational): Rational =
+      this + (-that)
+
+    @targetName("multiplication")
+    infix def *(that: Rational): Rational =
+      Rational(this.numer * that.numer, this.denom * that.denom)
+
+    @targetName("devision")
+    infix def /(that: Rational): Rational =
+      Rational(this.numer * that.denom, this.denom * that.numer)
+
+    override def toString: String = s"${this.numer}/${this.denom}"
+
+    private def gcd(a: Int, b: Int): Int =
+      if b == 0 then a else gcd(b, a % b)
+
+    private lazy val g = gcd(abs(x), y)
+
+    override def equals(other: Any): Boolean = // переопределение метода сравнения объекта на равенство
+      other match {
+        case that: Rational => this.numer == that.numer && this.denom == that.denom
+        case _ => false
+      }
+
+  end Rational
+
+end Homework
---
+++ homework4.scala
@@ -0,0 +1,101 @@
+import scala.annotation.targetName
+import karazin.scala.users.group.week4.utils.ItemOrdering
+
+object Homework:
+
+  abstract class IntSet:
+
+    infix def include(x: Int): IntSet
+
+    infix def remove(x: Int): IntSet
+
+    infix def contains(x: Int): Boolean
+
+    @targetName("union")
+    infix def ∪(that: IntSet): IntSet
+
+    @targetName("intersection")
+    infix def ∩(that: IntSet): IntSet
+
+    @targetName("complement")
+    infix def ∖(that: IntSet): IntSet
+
+    @targetName("disjunctive union")
+    infix def ∆(that: IntSet): IntSet
+
+  end IntSet
+
+  type Empty = Empty.type
+  
+  case object Empty extends IntSet:
+    
+    infix def include(x: Int): IntSet = NonEmpty(x, Empty, Empty)
+
+    infix def contains(x: Int): Boolean = false
+
+    infix def remove(x: Int): IntSet = Empty
+    
+    @targetName("union")
+   infix def ∪(that: IntSet): IntSet = that
+
+    @targetName("intersection")
+    infix def ∩(that: IntSet): IntSet = Empty
+
+    @targetName("complement")
+    infix def ∖(that: IntSet): IntSet = Empty
+
+    @targetName("disjunctive union")
+    infix def ∆(that: IntSet): IntSet = that
+    
+    override def toString: String = "[*]"    
+      
+    override def equals(other: Any): Boolean = false    
+  
+
+  end Empty
+    
+  case class NonEmpty(elem: Int, left: IntSet, right: IntSet) extends IntSet:
+
+    infix def include(x: Int): IntSet = 
+      if x < elem       then NonEmpty(elem, left include x, right)
+      else if x > elem  then NonEmpty(elem, left, right include x)
+      else              this
+
+    infix def contains(x: Int): Boolean = 
+      if x < elem       then left contains x
+      else if x > elem  then right contains x
+      else              true
+
+    // Optional task
+    infix def remove(x: Int): IntSet = 
+      if (x < elem)       then NonEmpty(elem, left.remove(x), right)
+      else if (x > elem)  then NonEmpty(elem, left, right.remove(x))
+      else left ∪ right
+
+    @targetName("union")
+    infix def ∪(that: IntSet): IntSet = left ∪ right ∪ that.include(elem)
+
+    @targetName("intersection")
+     infix def ∩(that: IntSet): IntSet = 
+      if (that contains elem) then NonEmpty(elem, left ∩ that, right ∩ that)
+      else left ∩ that ∪ right ∩ that
+
+    @targetName("complement")
+    infix def ∖(that: IntSet): IntSet = 
+      if (that contains elem) left ∖ that ∪ right ∖ that
+      else NonEmpty(elem, left ∖ that, right ∖ that)
+
+    @targetName("disjunctive union")
+    infix def ∆(that: IntSet): IntSet = 
+      (this ∖ that) ∪ (that ∖ this)
+    
+    override def toString: String = s"[$left - [$elem] - $right]"    
+    
+    override def equals(other: Any): Boolean = other match {
+      case NonEmpty(e, l, r) => elem == e && left == l && right == r
+      case _ => false
+    }
+
+  end NonEmpty
+
+end Homework
